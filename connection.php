6a5be6e84ab39a25e4dca5722ad39af2d0f1a92c
Patrick Muiruri <patrick.muiruri@strathmore.edu>
2023-06-29 19:13:31 +0300

    Web Dev 29/6/23 changes

--- connection.php
+++ connection.php
@@ -10,7 +10,18 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+else{
+  $stmt = $con->prepare("select * from drugdispensings where email = ?");
+  $stmt->bind_param("s", email);
+  $stmt->execute();
+  $stmt_result - $stmt->get_result();
+  if($stmt_result->num_rows = 0){
+    $data = $stmt_result->fetch_assoc();
+    if($data['password'] === $password){
+      echo "<h2>Login Successful</h2>"
+    }
+  }
+}
 /*$sql = "SELECT * from patient";
 $result = $conn ->query($sql);
 print_r($result);*/
--- processform.php
+++ processform.php
@@ -1,15 +1,61 @@
 <?php
-if (isset($_POST['Register'])) {
-$ssn = $_POST["ssn"];
+require_once("connection.php");
+if(isset($_GET['ssn'])){
+
+    $ssn = $_GET['ssn'];
+
+}else{
+
+    $ssn = " 001 - This is our default ssn";
+
+}
+if(isset($_GET['name'])){
+
+    $name = $_GET['name'];
+
+}else{
+
+    $name = " John - This is our default name";
+
+}
+if(isset($_GET['age'])){
+
+    $age = $_GET['age'];
+
+}else{
+
+    $age = " 20 - This is our default age";
+
+}
+if(isset($_GET['email'])){
+
+    $email = $_GET['email'];
+
+}else{
+
+    $email = " Johnmutansa@proton.me - This is our default email";
+
+}
+if(isset($_GET['password'])){
+
+    $password = $_GET['password'];
+
+}else{
+
+    $password = " John - This is our default password";
+
+}
+
+/*$ssn = $_POST["ssn"];
 $name = $_POST["name"];
 $age = $_POST["age"];
 $email = $_POST["email"];
-$password = $_POST["password"];
-}
+$password = $_POST["Password"];*/
+
 
 
 $host = "localhost";
-$dbname = "drugdispensing";
+$dbname = "drugdispensings";
 $username = "root";
 $password = "1Rurilongstaff1";
         
@@ -20,7 +66,7 @@ $conn = mysqli_connect(hostname: $host,
         
 if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
-}           
+}          
         
 $sql = "INSERT INTO patient (ssn, name, age, email, password)
         VALUES (?, ?, ?, ?, ?)";
@@ -34,14 +80,14 @@ if ( ! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_error($conn));
 }
 
-/*mysqli_stmt_bind_param($stmt, "isiss",
+mysqli_stmt_bind_param($stmt, "isiss",
                        $ssn,
                        $name,
                        $age,
                        $email,
-                       $password);*/
+                       $password);
 
-                       $stmt -> bind_param("isiss", $ssn, $name, $age, $email, $password);
+                      /* $stmt -> bind_param("isiss", $ssn, $name, $age, $email, $password);
                        $ssn = 1;
                        $name = 'Shikhar';
                        $age = 20;
@@ -51,7 +97,7 @@ if ( ! mysqli_stmt_prepare($stmt, $sql)) {
                        //Closing the statement
                        $stmt->close();
                        //Closing the connection
-                       $conn->close();
-//mysqli_stmt_execute($stmt);
+                       $conn->close();*/
+mysqli_stmt_execute($stmt);
 
 echo "Record saved.";
